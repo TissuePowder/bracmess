9cd4311674f9497023c8bc1cb8c58c1b6d6e53ff
TissuePowder <nillyhan.xt@gmail.com>
2022-04-18 18:46:50 +0600

    Changed morgan logging from dev to short

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ const mealsRouter = require('./routes/meals');
 require('dotenv').config();
 
 const app = express();
-
+app.enable("trust proxy");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(logger('short'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
